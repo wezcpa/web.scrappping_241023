39fdeb30e59cec66f3aea44dd81c52ea037007e9
wezcpa <wezcpa@kakao.com>
2024-10-23 15:58:11 +0900

    221023

--- 241022_selenium 입문.py
+++ 241022_selenium 입문.py
@@ -39,3 +39,13 @@ time.sleep(2)
 # 스크린샷 저장하고 창닫기
 driver.save_screenshot("naver_인공지능.png")
 driver.quit()
+
+'''Open git bash here에서 깃허브에 원격저장하기
+$ git init
+$ git remote add origin https://github.com/wezcpa/web.scrappping_241023
+$ git config --global user.name wezcpa
+$ git config --global user.email wezcpa@kakao.com
+$ git add .
+$ git commit -m "세팅"
+$ git push origin master
+'''
